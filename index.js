10fe9b8231a2e5cc9efa97952cab133eb3e2f081
Steven <steven@ceriously.com>
2018-07-12 21:26:58 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const { createServer } = require('http');
+const { parse } = require('url');
+const { fighter } = require("mma");
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+let cache = new Map();
+
+const server = createServer((req, res) => {
+    let { httpVersion, method, url } = req;
+    console.log(`${httpVersion} ${method} ${url}`);
+    let { pathname = '/', query = {} } = parse(url || '', true);
+
+    if (!query.fighter) {
+        const data = { error: 'Fighter Not Found' };
+        res.statusCode = 400;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(data, null, ' '));
+        return;
+    }
+
+    if (cache.has(query.fighter)) {
+        const data = cache.get(query.fighter);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(data, null, ' '));
+        return;
+    }
+
+    fighter(query.fighter, data => {
+        cache.set(query.fighter, data);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(data, null, ' '));
+    });
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
